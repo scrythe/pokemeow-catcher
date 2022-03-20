78a2beb4a2b25ac9b22ae27e0eefba44b704f611
Magomed <scrythepvp@gmail.com>
2022-03-20 15:35:13 +0100

    feat: automatically open pokemon channel

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,7 @@ const __dirname = dirname(__filename);
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await loginToDiscord(page, email, password);
+  await openDiscordPokemonChannel(page);
 })();
 
 interface User {
@@ -41,4 +42,22 @@ async function loginToDiscord(
     '#app-mount > div.app-3xd6d0 > div > div > div > div > form > div > div > div.mainLoginContainer-wHmAjP > div.block-3uVSn4.marginTop20-2T8ZJx > button.marginBottom8-emkd0_.button-1cRKG6.button-f2h6uQ.lookFilled-yCfaCM.colorBrand-I6CyqQ.sizeLarge-3mScP9.fullWidth-fJIsjq.grow-2sR_-F';
   await page.waitForSelector(loginButtonSelector);
   await page.click(loginButtonSelector);
+}
+
+async function openDiscordPokemonChannel(page: Page): Promise<void> {
+  const serverBtnSelector =
+    '#app-mount > div.app-3xd6d0 > div > div.layers-OrUESM.layers-1YQhyW > div > div > nav > ul > div.scroller-3X7KbA.none-2-_0dP.scrollerBase-_bVAAt > div:nth-child(4) > div:nth-child(1) > div:nth-child(2) > div > div > svg > foreignObject > div';
+  await page.waitForSelector(serverBtnSelector);
+  await page.click(serverBtnSelector);
+  await page.waitForSelector('#channels');
+  await page.evaluate(() => {
+    const scroll = document.querySelector('#channels');
+    const scrollHeight = scroll?.scrollHeight || 0;
+    scroll?.scrollBy(0, scrollHeight);
+  });
+  await page.waitForTimeout(1000);
+  const pokemonChannelSelector =
+    '#channels > ul > li:nth-child(19) > div > div > a > div.name-28HaxV.overflow-1wOqNV > div';
+  await page.waitForSelector(pokemonChannelSelector);
+  await page.click(pokemonChannelSelector);
 }

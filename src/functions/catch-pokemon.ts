1edd9fdea914660d4b5992086c70eca63d03b0d8
Magomed <scrythepvp@gmail.com>
2022-03-22 10:17:25 +0100

    feat: catch multiple pokemon through start

--- src/functions/catch-pokemon.ts
+++ src/functions/catch-pokemon.ts
@@ -6,23 +6,33 @@ async function typeP(
 ): Promise<void> {
   await page.type(typeMessageFieldSelecor, ';p');
   await page.keyboard.press('Enter');
-  await page.waitForTimeout(1000);
+  await page.waitForTimeout(2000);
 }
 
 async function catchPokemon(page: Page): Promise<void> {
-  const messagesXPath = '//*[@aria-label="Messages in pokemon"]/li';
-  const lastMessageXPath = `${messagesXPath}[last()]`;
-  const pokemonButtonXPath = `${lastMessageXPath}/div/div/div/div/div/button[1]`;
-  await page.waitForXPath(pokemonButtonXPath);
-  const [pokemonButton] = await page.$x(pokemonButtonXPath);
-  await pokemonButton.click();
+  await page.evaluate(() => {
+    const messages = document.querySelectorAll(
+      '[aria-label="Messages in pokemon"] li'
+    );
+    const message = messages[messages.length - 1];
+    const pokeballButton = message.querySelector(
+      'div > div > div > div > div > button:nth-child(1)'
+    ) as HTMLElement;
+    pokeballButton.click();
+  });
+  await page.waitForTimeout(4200);
 }
 
-export async function startPokemonCatch(page: Page): Promise<void> {
+export async function startPokemonCatch(
+  page: Page,
+  iterations: number
+): Promise<void> {
   const typeMessageFieldSelecor =
     '#app-mount > div.app-3xd6d0 > div > div.layers-OrUESM.layers-1YQhyW > div > div > div > div.content-1SgpWY > div.chat-2ZfjoI > div.content-1jQy2l > main > form > div > div > div > div.scrollableContainer-15eg7h.webkit-QgSAqd > div > div.textArea-2CLwUE.textAreaSlate-9-y-k2.slateContainer-3x9zil > div.markup-eYLPri.slateTextArea-27tjG0.fontSize16Padding-XoMpjI > div';
   await page.waitForSelector(typeMessageFieldSelecor);
-  await typeP(page, typeMessageFieldSelecor);
-  await catchPokemon(page);
-  await page.waitForTimeout(6900);
+  for (let i = 1; i < iterations; i++) {
+    await typeP(page, typeMessageFieldSelecor);
+    await catchPokemon(page);
+    await page.waitForTimeout(6900);
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import { startPokemonCatch } from './functions/catch-pokemon.js';
 import { initializeEnviroment } from './functions/initialize-enviroment.js';
 
 (async () => {
-  const { page } = await initializeEnviroment({ newTab: false });
-  await startPokemonCatch(page);
+  const { browser, page } = await initializeEnviroment({ newTab: true });
+  await startPokemonCatch(page, 9);
+  await browser.close();
   console.log('done');
 })();
